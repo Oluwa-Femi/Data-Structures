c16c9b1a66aeff9d84cfa4ecba07592b5aaf164d
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-06-10 12:20:13 +0100

    Refactored stack

--- stack/stack.py
+++ stack/stack.py
@@ -14,18 +14,34 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList()
+        self.storage = []
 
     def __len__(self):
         return (self.size)
 
     def push(self, value):
-        self.storage.add_to_tail(value)
         self.size += 1
+        self.storage.insert(len(self.storage) + 1, value)
 
     def pop(self):
-        if self.size is not 0:
-            self.size -= 1
-            return self.storage.remove_tail()
-        else:
+        if len(self.storage) == 0:
+            return
+        self.size -= 1
+        return self.storage.pop()
+        
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+    def pop(self):
+        if self.size == 0:
             return None
+        self.size -= 1
+        return self.storage.remove_tail()
